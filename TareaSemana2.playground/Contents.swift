78acf19113acceb898ada6c5fffff22681057f76
Angye <Nailim@macbook-air-de-angelica.local>
2016-01-19 00:43:32 -0600

    TareaFinalizada

---
+++ TareaSemana2.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for var i = 1; i<=100; i+=1{
+    if i % 5 == 0 {
+        print("\(i) - Bingo!!!")
+    }
+    if i % 2 == 0{
+        print("\(i) - Par!!!")
+    }
+    if i % 2 != 0{
+        print("\(i) - Impar!!!")
+    }
+    if i >= 30 && i<=40{
+        print("\(i) - Viva Swift!!!")
+    }
+    
+}
